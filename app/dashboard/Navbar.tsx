9cb40ba0b8bad851826400e3d706e2ca54361fd5
Deepanshu Pal <dp5114@dseu.ac.in>
2024-04-18 00:31:51 +0530

    made navbar

--- app/dashboard/Navbar.tsx
+++ app/dashboard/Navbar.tsx
@@ -1,21 +1,20 @@
   "use client";
   import React from "react";
-  import { Inter } from "next/font/google";
-
-  const inter = Inter({ subsets: ["latin"] });
 
   interface Props {
-    selected: string
+    selected: Number,
+    setSelected: Function
   }
-  export default function Navbar({selected}:Props) {
-    const options = ["Dashboard", "Profile", "Exam Registration"]
+  export default function Navbar({selected,setSelected}:Props) {
+    const options = ["Dashboard", "Profile", "Exam Registration", "Course Details"]
     return (
       <>
-        <div className={` w-[250px] text-lg font-light px-8 py-48 space-y-10  h-full shadow-2xl shadow-slate-200 -z-20 bg-white absolute top-[60px] border-2  left-0`}>
-          <div className="cursor-pointer hover:text-blue-600  "> Dashboard </div>
-          <div className="cursor-pointer hover:text-blue-600  "> Profile </div>
-          <div className="cursor-pointer hover:text-blue-600  "> Exam Registration </div>
-          <div className="cursor-pointer hover:text-blue-600  "> Course Details </div>
+        <div className={` w-[250px] text-lg font-normal px-8 py-48 space-y-10  h-full shadow-2xl shadow-slate-200 -z-20 bg-white absolute top-[60px] border-2  left-0`}>
+          {options.map((option,index)=>{
+            return (
+              <div key={index} className={`cursor-pointer hover:text-blue-600 ${selected===index?'text-blue-600':''} `}> {option}</div>
+            )
+          })}          
         </div>
       </>
     );
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,15 @@
+"use client"
+import { useState } from "react";
 import Header from "./Header";
 import Navbar from "./Navbar";
 
 export default function Home() {
+  const [selected, setSelected] = useState(0);
   return (
     <>
       <div className="bg-[#dfdede] ">
         <Header username={"Abhinav M"} />
-        <Navbar />
+        <Navbar selected={selected} setSelected={setSelected} />
       </div>
     </>
   );
