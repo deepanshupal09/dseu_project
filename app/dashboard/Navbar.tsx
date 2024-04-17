c13a6bdea0ec24f3310c0f1acc055176e320bda7
cannabinoider <shubhamv23030@gmai1.com>
2024-04-18 05:13:14 +0530

    dashboard initialized

--- app/dashboard/Navbar.tsx
+++ app/dashboard/Navbar.tsx
@@ -9,10 +9,10 @@
     const options = ["Dashboard", "Profile", "Exam Registration", "Course Details"]
     return (
       <>
-        <div className={` w-[250px] text-lg font-normal px-8 py-48 space-y-10  h-full shadow-2xl shadow-slate-200 -z-20 bg-white absolute top-[60px] border-2  left-0`}>
+        <div className={` w-[250px] text-xl font-normal px-8 py-48 space-y-10  h-full shadow-2xl shadow-slate-200 -z-20 bg-white absolute top-[60px] border-2  left-0`}>
           {options.map((option,index)=>{
             return (
-              <div key={index} className={`cursor-pointer hover:text-blue-600 ${selected===index?'text-blue-600':''} `}> {option}</div>
+              <div key={index} className={`cursor-pointer hover:text-dseublue ${selected===index?'text-dseublue':''} `}> <strong>{option}</strong></div>
             )
           })}          
         </div>
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import Header from "./Header";
 import Navbar from "./Navbar";
+import CampaignIcon from '@mui/icons-material/Campaign';
 
 export default function Home() {
   const [selected, setSelected] = useState(0);
@@ -10,6 +11,18 @@ export default function Home() {
       <div className="bg-[#dfdede] ">
         <Header username={"Abhinav M"} />
         <Navbar selected={selected} setSelected={setSelected} />
+      </div>
+      <div className="welcome py-2 px-4 rounded shadow absolute top-[150px] left-[250px] right-0 z-10 mx-12 ">
+        <h1 className="text-2xl text-white-800 font-bold ">Welcome {"Abhinav M"}</h1>
+      </div>
+      <div className="announcement bg-dseublue py-2 px-4 rounded shadow absolute top-[200px] left-[250px] right-0 z-10 mx-12">
+        <h1 className="text-xl text-white font-bold"><CampaignIcon className="text-3xl"/>Announcement</h1>
+          <ul className="text-white">
+            <li  className="my-6">Examination Registerations about to close.</li>
+            <li className="my-6">Course Details about to open.</li>
+            <li className="my-6">Backlog exams for 2nd 4th semester.</li>
+            <li className="my-6">More information.</li>
+          </ul>
       </div>
     </>
   );
