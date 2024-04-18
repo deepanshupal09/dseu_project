5b9505a616cd9688a4df72b747fcb462bc4a9897
cannabinoider <shubhamv23030@gmai1.com>
2024-04-18 21:02:12 +0530

    responsive navbar

--- app/dashboard/Navbar.tsx
+++ app/dashboard/Navbar.tsx
@@ -1,21 +1,21 @@
-  "use client";
-  import React from "react";
+"use client";
+import React from "react";
 
-  interface Props {
-    selected: Number,
-    setSelected: Function
-  }
-  export default function Navbar({selected,setSelected}:Props) {
-    const options = ["Dashboard", "Profile", "Exam Registration", "Course Details"]
-    return (
-      <>
-        <div className={` w-[250px] text-xl font-normal px-8 py-48 space-y-10  h-full shadow-2xl shadow-slate-200 -z-20 bg-white absolute top-[60px] border-2  left-0`}>
-          {options.map((option,index)=>{
-            return (
-              <div key={index} className={`cursor-pointer hover:text-dseublue ${selected===index?'text-dseublue':''} `}> <strong>{option}</strong></div>
-            )
-          })}          
-        </div>
-      </>
-    );
-  }
+interface Props {
+  selected: Number,
+  setSelected: Function
+}
+export default function Navbar({selected,setSelected}:Props) {
+  const options = ["Dashboard", "Profile", "Exam Registration", "Course Details"]
+  return (
+    <>
+      <div className={` w-[250px] text-xl font-normal px-8 py-48 space-y-10  h-full shadow-2xl shadow-slate-200 -z-20 bg-white absolute top-[60px] border-2  left-0`}>
+        {options.map((option,index)=>{
+          return (
+            <div key={index} className={`cursor-pointer hover:text-dseublue ${selected===index?'text-dseublue':''} `}> <strong>{option}</strong></div>
+          )
+        })}          
+      </div>
+    </>
+  );
+}
