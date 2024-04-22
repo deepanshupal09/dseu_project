51694e022a88a943d36d41eac246807c82e2af17
cannabinoider <shubhamv23030@gmai1.com>
2024-04-23 01:48:03 +0530

    exam-registeration fixed

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -31,6 +31,8 @@ interface PersonalDetails {
   guardianNameInput: string;
   gender: string;
   genderInput: string;
+  category: string;
+  categoryInput: string;
   mobileNumber: string;
   mobileNumberInput: string;
   email: string;
