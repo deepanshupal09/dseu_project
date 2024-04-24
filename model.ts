81a138f81c7b05068ba3d01155cbd38ac7c3cbf9
anant <ab6157@dseu.ac.in>
2024-04-24 17:29:43 +0530

    fixed

--- model.ts
+++ model.ts
@@ -3,156 +3,156 @@
 import { QueryResult } from "pg";
 import pool from "./db";
 import {
-  getPasswordByRollno,
-  putToken,
-  fetchToken,
-  pushTokenQuery,
-  updateDetailsByRollno,
-  getUserByRollno,
-  // addExamRegisterationByRollno,
-  fetchCoursesBySemester,
-  fetchCoursesByRollNo,
-  fetchExamRegistrationByRollNo,
-  fetchExamRegistrationByCourseCode,
-  fetchExamRegistrationByProgramAndSemester,
+    getPasswordByRollno,
+    putToken,
+    fetchToken,
+    pushTokenQuery,
+    updateDetailsByRollno,
+    getUserByRollno,
+    // addExamRegisterationByRollno,
+    fetchCoursesBySemester,
+    fetchCoursesByRollNo,
+    fetchExamRegistrationByRollNo,
+    fetchExamRegistrationByCourseCode,
+    fetchExamRegistrationByProgramAndSemester,
 } from "./queries";
 
 export function fetchPasswordByRollNo(
-  rollno: string
+    rollno: string
 ): Promise<QueryResult<any>> {
-  return new Promise((resolve, reject) => {
-    pool.query(getPasswordByRollno, [rollno], (error, results) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(results);
-      }
+    return new Promise((resolve, reject) => {
+        pool.query(getPasswordByRollno, [rollno], (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
     });
-  });
 }
 
 export function updateToken(
-  token: string,
-  rollno: string,
-  last_modified: string,
-  expiry: string
+    token: string,
+    rollno: string,
+    last_modified: string,
+    expiry: string
 ): Promise<QueryResult<any>> {
-  return new Promise((resolve, reject) => {
-    pool.query(
-      putToken,
-      [token, last_modified, expiry, rollno],
-      (error, results) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(results);
-        }
-      }
-    );
-  });
+    return new Promise((resolve, reject) => {
+        pool.query(
+            putToken,
+            [token, last_modified, expiry, rollno],
+            (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            }
+        );
+    });
 }
 
 export function fetchTokenByRollNo(rollno: string): Promise<QueryResult<any>> {
-  return new Promise((resolve, reject) => {
-    pool.query(fetchToken, [rollno], (error, results) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(results);
-      }
+    return new Promise((resolve, reject) => {
+        pool.query(fetchToken, [rollno], (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
     });
-  });
 }
 
 export function pushToken(
-  rollno: string,
-  token: string,
-  created_at: string,
-  last_modified: string,
-  expiry: string
+    rollno: string,
+    token: string,
+    created_at: string,
+    last_modified: string,
+    expiry: string
 ): Promise<QueryResult<any>> {
-  return new Promise((resolve, reject) => {
-    pool.query(
-      pushTokenQuery,
-      [rollno, token, created_at, last_modified, expiry],
-      (error, results) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(results);
-        }
-      }
-    );
-  });
+    return new Promise((resolve, reject) => {
+        pool.query(
+            pushTokenQuery,
+            [rollno, token, created_at, last_modified, expiry],
+            (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            }
+        );
+    });
 }
 
 export function putDetailsByRollno(
-  rollno: string,
-  program: string,
-  semester: number,
-  phone: string,
-  campus: string,
-  emailid: string,
-  gender: string,
-  alternate_phone: string,
-  father: string,
-  mother: string,
-  guardian: string,
-  aadhar: string,
-  abc_id: string,
-  pwbd_certificate: string|null,
-  photo: string|null,
-  last_modified: string,
-  program_type: string,
-  password: string
+    rollno: string,
+    program: string,
+    semester: number,
+    phone: string,
+    campus: string,
+    emailid: string,
+    gender: string,
+    alternate_phone: string,
+    father: string,
+    mother: string,
+    guardian: string,
+    aadhar: string,
+    abc_id: string,
+    pwbd_certificate: string,
+    photo: string,
+    last_modified: string,
+    program_type: string,
+    password: string
 ): Promise<QueryResult<any>> {
-  console.log("here");
-  return new Promise((resolve, reject) => {
-    pool.query(
-      updateDetailsByRollno,
-      [
-        program,
-        semester,
-        phone,
-        campus,
-        emailid,
-        gender,
-        alternate_phone,
-        father,
-        mother,
-        guardian,
-        aadhar,
-        abc_id,
-        pwbd_certificate,
-        photo,
-        last_modified,
-        program_type,
-        password,
-        rollno
-      ],
-      (error, results) => {
-        if (error) {
-            console.log("model", error)
-          reject(error);
-        } else {
-          resolve(results);
-        }
-      }
-    );
-  });
+    console.log("here");
+    return new Promise((resolve, reject) => {
+        pool.query(
+            updateDetailsByRollno,
+            [
+                program,
+                semester,
+                phone,
+                campus,
+                emailid,
+                gender,
+                alternate_phone,
+                father,
+                mother,
+                guardian,
+                aadhar,
+                abc_id,
+                pwbd_certificate,
+                photo,
+                last_modified,
+                program_type,
+                password,
+                rollno,
+            ],
+            (error, results) => {
+                if (error) {
+                    console.log("model", error);
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            }
+        );
+    });
 }
 
-export function fetchUser ( rollno:string ) : Promise<QueryResult<any>> { 
-  return new Promise(( resolve, reject )=> {
-    pool.query(getUserByRollno, [rollno], (error, results) => {
-      if (error) {
-        console.log("Model error: ",error);
-        reject(error);
-      } else{
-        resolve(results);
-      }
+export function fetchUser(rollno: string): Promise<QueryResult<any>> {
+    return new Promise((resolve, reject) => {
+        pool.query(getUserByRollno, [rollno], (error, results) => {
+            if (error) {
+                console.log("Model error: ", error);
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
     });
-  })
 }
 
 // export function addExamRegisteration ( rollno:string, course_code:string, last_modified:string ): Promise<QueryResult<any>> {
@@ -168,103 +168,136 @@ export function fetchUser ( rollno:string ) : Promise<QueryResult<any>> {
 //   })
 // }
 
-
-export function fetchCourses(semester: number, program: string): Promise<QueryResult<any>> {
-  return new Promise((resolve, reject) => {
-    pool.query(fetchCoursesBySemester, [semester, program], (error, results) => {
-      if (error) {
-        console.log("fetch courses error: ",error);
-        reject(error);
-      } else {
-        resolve(results);
-      }
+export function fetchCourses(
+    semester: number,
+    program: string
+): Promise<QueryResult<any>> {
+    return new Promise((resolve, reject) => {
+        pool.query(
+            fetchCoursesBySemester,
+            [semester, program],
+            (error, results) => {
+                if (error) {
+                    console.log("fetch courses error: ", error);
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            }
+        );
     });
-  });
 }
 
 export function fetchCoursesRollNo(rollno: string): Promise<QueryResult<any>> {
-  return new Promise((resolve, reject) => {
-    pool.query(fetchCoursesByRollNo, [rollno], (error, results) => {
-      if (error) {
-        console.log("fetch courses by roll no error: ", error);
-        reject(error);
-      } else {
-        resolve(results);
-      }
+    return new Promise((resolve, reject) => {
+        pool.query(fetchCoursesByRollNo, [rollno], (error, results) => {
+            if (error) {
+                console.log("fetch courses by roll no error: ", error);
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
     });
-  });
 }
 
-export function fetchExamRegistration( rollno:string ): Promise<QueryResult<any>> {
-  return new Promise((resolve, reject) => {
-    pool.query(fetchExamRegistrationByRollNo, [rollno], (error, results) => {
-      if(error) {
-        reject(error);
-      } else{
-        resolve(results);
-      }
-    })
-  })
+export function fetchExamRegistration(
+    rollno: string
+): Promise<QueryResult<any>> {
+    return new Promise((resolve, reject) => {
+        pool.query(
+            fetchExamRegistrationByRollNo,
+            [rollno],
+            (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            }
+        );
+    });
 }
 
-export function fetchExamRegistrationCourse( course_code:string) :Promise<QueryResult<any>> {
-  return new Promise ((resolve, reject) => {
-    pool.query(fetchExamRegistrationByCourseCode, [course_code], (error, results)=> {
-      if(error) {
-        reject(error);
-      } else{
-        resolve(results);
-      }
-    })
-  })
+export function fetchExamRegistrationCourse(
+    course_code: string
+): Promise<QueryResult<any>> {
+    return new Promise((resolve, reject) => {
+        pool.query(
+            fetchExamRegistrationByCourseCode,
+            [course_code],
+            (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            }
+        );
+    });
 }
 
-export function fetchExamRegistrationProgramAndSemester( program:string ,semester:number) :Promise<QueryResult<any>> {
-  return new Promise ((resolve, reject) => {
-    pool.query(fetchExamRegistrationByProgramAndSemester, [program ,semester], (error, results)=> {
-      if(error) {
-        reject(error);
-      } else{
-        resolve(results);
-      }
-    })
-  })
+export function fetchExamRegistrationProgramAndSemester(
+    program: string,
+    semester: number
+): Promise<QueryResult<any>> {
+    return new Promise((resolve, reject) => {
+        pool.query(
+            fetchExamRegistrationByProgramAndSemester,
+            [program, semester],
+            (error, results) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(results);
+                }
+            }
+        );
+    });
 }
 
-export function insertUsers(users: Array<{ rollno: string, name: string, password: string }>): Promise<QueryResult<any>> {
-  return new Promise((resolve, reject) => {
-    const query = `
+export function insertUsers(
+    users: Array<{ rollno: string; name: string; password: string }>
+): Promise<QueryResult<any>> {
+    return new Promise((resolve, reject) => {
+        const query = `
       INSERT INTO users (rollno, name, password)
-      VALUES ${users.map(user => {
-        return `('${user.rollno}', '${user.name}', '${user.password}')`;
-      }).join(', ')}
+      VALUES ${users
+          .map((user) => {
+              return `('${user.rollno}', '${user.name}', '${user.password}')`;
+          })
+          .join(", ")}
     `;
-    pool.query(query, (error, results) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(results);
-      }
+        pool.query(query, (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
     });
-  });
 }
 
-
-export function insertExamRegisterations(registeration: { rollno: string, course_code: Array<string>}): Promise<QueryResult<any>> {
-  return new Promise((resolve, reject) => {
-    const last_modified: string = new Date().toString();
-    const query = `
+export function insertExamRegisterations(registeration: {
+    rollno: string;
+    course_code: Array<string>;
+}): Promise<QueryResult<any>> {
+    return new Promise((resolve, reject) => {
+        const last_modified: string = new Date().toString();
+        const query = `
       INSERT INTO exam_registeration (rollno, course_code, last_modified)
-      VALUES ${registeration.course_code.map(courseCode => {
-        return `('${registeration.rollno}', '${courseCode}', '${last_modified}')`;
-      }).join(', ')}
+      VALUES ${registeration.course_code
+          .map((courseCode) => {
+              return `('${registeration.rollno}', '${courseCode}', '${last_modified}')`;
+          })
+          .join(", ")}
     `;
-    pool.query(query, (error, results) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(results);
-      }
+        pool.query(query, (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
     });
-  });
 }
