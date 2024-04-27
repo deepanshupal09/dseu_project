2e306fa02646d4020dcff3dcbc9cb6a6105e7841
Deepanshu Pal <dp5114@dseu.ac.in>
2024-04-28 03:37:52 +0530

    logo added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import favicon from "../public/favicon.ico"
+
 
 
 export const metadata: Metadata = {
@@ -13,7 +15,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">      
+    <html lang="en"> 
+      <head>
+      <link
+            rel="icon"
+            href={favicon.src}
+        />
+        </head>     
       <body>{children}</body>      
     </html>
   );
