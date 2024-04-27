8759f17f96e8e5bba66abd7c2b10d44f00340b65
deepanshupal09 <dp5114@dseu.ac.in>
2024-04-28 02:50:40 +0530

    change

--- app/getuserdetails/[rollno]/page.tsx
+++ app/getuserdetails/[rollno]/page.tsx
@@ -17,7 +17,7 @@ import Final from "./Final";
 import PreviewPage from "./Preview";
 
 export default function Home() {
-  const [step, setStep] = useState<number>(7);
+  const [step, setStep] = useState<number>(1);
   const [newpass, setNewpass] = useState<string>("");
   const [confirm, setConfirm] = useState<string>("");
   const [emailid, setEmailid] = useState<string>("");
@@ -59,7 +59,7 @@ export default function Home() {
   };
 
   const handleNext = async () => {
-    if (step <= 8) {
+    if (step < 8) {
       setStep(step + 1);
       console.log(step, "step");
     } else {
