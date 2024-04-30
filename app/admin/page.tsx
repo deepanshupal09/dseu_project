8bf714538e04c2435c0d1e740070b8810eecb88d
Deepanshu Pal <dp5114@dseu.ac.in>
2024-04-30 17:39:54 +0530

    admin login added

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,15 +4,11 @@ import { Backdrop, CircularProgress, TextField } from "@mui/material";
 import { useState } from "react";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import Image from "next/image";
-import logo from "./images/logo.png";
+import logo from "../images/logo.png"
 import { useRouter } from "next/navigation";
-import { login } from "./actions/api";
-import { getAuth, setAuth, setSignupCookie } from "./actions/cookie";
-import jwt_decode, { JwtPayload } from "jwt-decode"; // Import JwtPayload type
-import { parseJwt } from "./actions/utils";
 
 export default function Home() {
-  const [RollNo, setRollNo] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
   const [Password, setPass] = useState<string>("");
   const [helperText, setHelperText] = useState<string>("");
   const [error, setError] = useState<boolean>(false);
@@ -22,38 +18,7 @@ export default function Home() {
 
 
   async function handleLogin() {
-    try {
-      setLoading(true);
-      const response = await login({ rollno: RollNo, password: Password });
-      console.log("response: ", response);
-      if (response === 400) {
-        setError(true);
-        setHelperText("Incorrect password");
-      } else if (response === 404) {
-        setError(true);
-        setHelperText("Roll No. Not found");
-      } else if (response === 500) {
-        setError(true);
-        setHelperText("Internal Server Error");
-      } else {
-        if (response.defaultPass) {
-          await setSignupCookie();
-          router.push(`/getuserdetails/${RollNo}`)
-        } else {
-          await setAuth(response.token);
-          router.push("/dashboard")
-        }
-        // const token = await getAuth();
-        // console.log("user: ", parseJwt(token?.value as string));
-      }
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-      // const errorData = await response.text();
-      setError(true);
-        setHelperText("Internal Server Error");
-      console.log("error: ", error);
-    }
+  
   }
 
   return (
@@ -68,17 +33,17 @@ export default function Home() {
             className="flex flex-col  bg-white  rounded-3xl shadow-2xl max-[450px]:backdrop-blur-0 max-[450px]:rounded-none shadow-slate-400 p-6 items-center space-y-10 justify-start h-fit pt-16 pb-10 px-10 max-[450px]:w-[100%] max-[450px]:h-[100%] w-[460px] ] "
           >
             <Image height={130} className="-mb-4" src={logo} alt="logo" />
-            <div className="text-[32px] font-semibold  ">Exam Portal</div>
-            <div className="text-2xl w-full font-semibold ">Login</div>
+            <div className="text-[32px] font-semibold  ">Admin Login</div>
+            {/* <div className="text-2xl w-full font-semibold ">Login</div> */}
             <div className="mt-1 w-[100%]">
               <TextField
                 required
                 onChange={(e) => {
-                  setRollNo(e.target.value);
+                  setEmail(e.target.value);
                   setHelperText("");
                   setError(false);
                 }}
-                value={RollNo}
+                value={email}
                 helperText={helperText}
                 error={error}
                 className=""
@@ -96,7 +61,7 @@ export default function Home() {
                   },
                 }}
                 id="myfilled-name"
-                label="Roll No"
+                label="Email ID"
                 variant="outlined"
                 color="grey"
                 fullWidth
@@ -136,8 +101,6 @@ export default function Home() {
               <ArrowForwardIosIcon className="scale-75  " />
             </button>
             <div className="flex w-full justify-between">
-            <div onClick={()=>{router.push("/admin")}} className="w-full  cursor-pointer hover:underline ">Are you an admin?</div>
-            <div onClick={()=>{router.push("/forgotpassword")}} className="w-full text-right cursor-pointer hover:underline">Forgot Password?</div>
             </div>
           </form>
         </div>
