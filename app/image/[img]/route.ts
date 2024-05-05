a1fc6a7d5026a6d2616d2167b6fbe0f944b1d23e
deepanshupal09 <dp5114@dseu.ac.in>
2024-05-05 17:27:21 +0530

    cors  fix

--- app/image/[img]/route.ts
+++ app/image/[img]/route.ts
@@ -5,10 +5,13 @@ export async function GET( request:Request, { params }: { params: { img: string 
   const { img } = params;
   const imagePath = path.join('/home/dseu/Desktop/uploads/',img);
 
+
   try {
     const fileData = fs.readFileSync(imagePath);
     return new Response(fileData, {
-      headers: { 'Content-Type': 'image/jpeg' },
+      headers: { 'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization', 'Content-Type': 'image/jpeg' },
     });
   } catch(error:any) {
 
