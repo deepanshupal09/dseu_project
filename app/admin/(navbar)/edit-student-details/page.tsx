42ae2d2ee59337dc3106e64fbba391fbfc851025
deepanshupal09 <dp5114@dseu.ac.in>
2024-05-28 10:14:09 +0530

    build

--- app/admin/(navbar)/edit-student-details/page.tsx
+++ app/admin/(navbar)/edit-student-details/page.tsx
@@ -42,36 +42,9 @@ import { useDebouncedCallback } from "use-debounce";
 import { DeleteForever } from "@mui/icons-material";
 
 import { useData } from "@/contexts/DataContext";
+import { deepEqual } from "@/utils";
 
-export function deepEqual(obj1: any, obj2: any): boolean {
-  if (obj1 === obj2) {
-    return true;
-  }
 
-  if (
-    obj1 == null ||
-    typeof obj1 !== "object" ||
-    obj2 == null ||
-    typeof obj2 !== "object"
-  ) {
-    return false;
-  }
-
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
-
-  for (const key of keys1) {
-    if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
-      return false;
-    }
-  }
-
-  return true;
-}
 
 function Home() {
   const [user, setUser] = useState<StudentDetails | null>(null);
@@ -648,5 +621,6 @@ function Home() {
     </div>
   );
 }
+export {deepEqual}
 
 export default Home;
