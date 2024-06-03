091390e2b5d63d0ae24b575ee304336a38e64c58
Deepanshu Pal <dp5114@dseu.ac.in>
2024-06-04 02:53:18 +0530

    initial menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href={favicon.src} />
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com"> */}
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+{/* <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet" /></link> */}
       </head>
       <body><DataProvider>{children}</DataProvider></body>
     </html>
