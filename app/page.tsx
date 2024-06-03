5e262e836be9b809656e0e64e49f04d84eb7c4a9
anant <ab6157@dseu.ac.in>
2024-06-04 01:50:28 +0530

    aboutus

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Backdrop, CircularProgress, TextField } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import Image from "next/image";
 import logo from "./images/logo.png";
@@ -9,138 +9,181 @@ import { useRouter } from "next/navigation";
 import { login } from "./actions/api";
 import { getAuth, setAuth, setSignupCookie } from "./actions/cookie";
 
-
 export default function Home() {
-  const [RollNo, setRollNo] = useState<string>("");
-  const [Password, setPass] = useState<string>("");
-  const [helperText, setHelperText] = useState<string>("");
-  const [error, setError] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(false);
-  const router = useRouter();
-  async function handleLogin() {
-    try {
-      setLoading(true);
-      const response = await login({ rollno: RollNo, password: Password });
-      if (response === 400) {
-        setError(true);
-        setHelperText("Incorrect password");
-      } else if (response === 404) {
-        setError(true);
-        setHelperText("Roll No. Not found");
-      } else if (response === 500) {
-        setError(true);
-        setHelperText("Internal Server Error");
-      } else {
-        
-        if (response.defaultPass) {
-          await setSignupCookie();
-          router.push(`/getuserdetails/${RollNo}`)
-        } else {
-          await setAuth(response.token);
-          router.push("/dashboard")
+    const [RollNo, setRollNo] = useState<string>("");
+    const [Password, setPass] = useState<string>("");
+    const [helperText, setHelperText] = useState<string>("");
+    const [error, setError] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
+    const router = useRouter();
+    async function handleLogin() {
+        try {
+            setLoading(true);
+            const response = await login({ rollno: RollNo, password: Password });
+            if (response === 400) {
+                setError(true);
+                setHelperText("Incorrect password");
+            } else if (response === 404) {
+                setError(true);
+                setHelperText("Roll No. Not found");
+            } else if (response === 500) {
+                setError(true);
+                setHelperText("Internal Server Error");
+            } else {
+                if (response.defaultPass) {
+                    await setSignupCookie();
+                    router.push(`/getuserdetails/${RollNo}`);
+                } else {
+                    await setAuth(response.token);
+                    router.push("/dashboard");
+                }
+            }
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+            // const errorData = await response.text();
+            setError(true);
+            setHelperText("Internal Server Error");
         }
-      }
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-      // const errorData = await response.text();
-      setError(true);
-        setHelperText("Internal Server Error");
     }
-  }
+    const [isOpen, setIsOpen] = useState(false);
+    const [startBottomAnimation, setStartBottomAnimation] = useState(false);
 
-  return (
-    <>
-      <div className=" ">
-        <div className="flex self-center justify-center min-h-[100vh]  items-center">
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              handleLogin();
-            }}
-            className="flex flex-col  bg-white  rounded-3xl shadow-2xl max-[450px]:backdrop-blur-0 max-[450px]:rounded-none shadow-slate-400 p-6 items-center max-md:space-y-7 md:space-y-10 justify-start h-fit pt-16 pb-10 px-10 max-[450px]:w-[100%] max-[450px]:h-[100vh] w-[460px] ] "
-          >
-            <Image height={130} className="-mb-4" src={logo} alt="logo" />
-            <div className="text-[32px] font-semibold  ">Exam Portal</div>
-            <div className="text-2xl w-full font-semibold ">Login</div>
-            <div className="mt-1 w-[100%]">
-              <TextField
-                required
-                onChange={(e) => {
-                  setRollNo(e.target.value);
-                  setHelperText("");
-                  setError(false);
-                }}
-                value={RollNo}
-                helperText={helperText}
-                error={error}
-                className=""
-                sx={{
-                  ".MuiInputBase-input": {
-                    borderRadius: "10px",
-                  },
-                  "&:before, &:after": {
-                    borderRadius: "10px",
-                  },
-                }}
-                InputProps={{
-                  style: {
-                    borderRadius: "10px",
-                  },
-                }}
-                id="myfilled-name"
-                label="Roll No"
-                variant="outlined"
-                color="grey"
-                fullWidth
-              />
+    useEffect(() => {
+        if (isOpen) {
+            // Start the bottom div's animation after a delay
+            const timer = setTimeout(() => {
+                setStartBottomAnimation(true);
+            }, 700); // Adjust the delay as needed
+
+            return () => clearTimeout(timer); // Cleanup timer on unmount
+        } else {
+            setStartBottomAnimation(false);
+        }
+    }, [isOpen]);
+
+    return (
+        <>
+            <div className="relative">
+                <div className="flex items-center fixed max-sm:top-2 max-sm:right-7 top-8 right-20 z-30">
+                    <button className="relative w-10 h-10" onClick={() => setIsOpen(!isOpen)} aria-label="Menu">
+                        <div className={`absolute top-1/2 left-1/2 w-8 h-[2px] bg-gray-300 transition-transform duration-300 ease-in-out ${isOpen ? "rotate-45" : "-translate-y-2"} transform origin-center`}></div>
+                        <div className={`absolute top-1/2 left-1/2 w-8 h-[2px] bg-gray-300 transition-opacity duration-300 ease-in-out ${isOpen ? "opacity-0" : ""}`}></div>
+                        <div className={`absolute top-1/2 left-1/2 w-8 h-[2px] bg-gray-300 transition-transform duration-300 ease-in-out ${isOpen ? "-rotate-45" : "translate-y-2"} transform origin-center`}></div>
+                    </button>
+                </div>
+                <div className={`fixed top-0 right-0 z-20 w-screen h-screen bg-black opacity-50  transition-transform duration-[300ms] ease-in-out ${isOpen ? "translate-y-0" : "-translate-y-full"}`}></div>
+                <div className={`fixed top-0 right-0 z-20 w-screen h-screen bg-black opacity-80 transition-transform duration-[450ms] ease-in-out ${isOpen ? "translate-y-0" : "-translate-y-full"}`}></div>
+                {/* <div className={`fixed top-0 right-0 z-20 w-screen h-screen bg-green-400 transition-transform duration-[400ms] ease-in-out ${isOpen ? "translate-y-0" : "-translate-y-full"}`}></div>
+                <div className={`fixed top-0 right-0 z-20 w-screen h-screen bg-orange-400  transition-transform duration-[450ms] ease-in-out ${isOpen ? "translate-y-0" : "-translate-y-full"}`}></div>
+                <div className={`fixed top-0 right-0 z-20 w-screen h-screen bg-red-400  transition-transform duration-[500ms] ease-in-out ${isOpen ? "translate-y-0" : "-translate-y-full"}`}></div> */}
+                <div className={`fixed top-0 right-0 z-20 w-screen h-screen bg-white transition-transform duration-[600ms] ease-in-out ${isOpen ? "translate-y-0" : "-translate-y-full"}`}>
+                    <div className={`h-[2px] fixed top-20 w-full bg-gray-300 transition-all duration-700 ease-in-out ${isOpen ? "w-full" : "w-0"}`}></div>
+                    <div className={`h-[2px] fixed bottom-20 w-full bg-gray-300 transition-all duration-700 ease-in-out ${isOpen ? "w-full" : "w-0"}`}></div>
+                    <div className={`fixed top-20 bottom-20 w-[2px] right-1/2 bg-gray-300 transition-all duration-1000 ease-in-out ${startBottomAnimation ? "h-[calc(100vh-160px)]" : "h-0"}`}></div>
+                    <div className={`fixed top-20  `}></div>
+                </div>
             </div>
-            <div className="mt-1 w-[100%]">
-              <TextField
-                required
-                onChange={(e) => {
-                  setPass(e.target.value);
-                  setHelperText("");
-                  setError(false);
-                }}
-                type="password"
-                value={Password}
-                helperText={helperText}
-                error={error}
-                sx={{
-                  "&:before, &:after": {
-                    borderRadius: "10px",
-                  },
-                }}
-                InputProps={{
-                  style: {
-                    borderRadius: "10px",
-                  },
-                }}
-                id="myfilled-name"
-                label="Password"
-                variant="outlined"
-                color="grey"
-                fullWidth
-              />
+            <div className=" ">
+                <div className="flex self-center justify-center min-h-[100vh]  items-center">
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            handleLogin();
+                        }}
+                        className="flex flex-col  bg-white  rounded-3xl shadow-2xl max-[450px]:backdrop-blur-0 max-[450px]:rounded-none shadow-slate-400 p-6 items-center max-md:space-y-7 md:space-y-10 justify-start h-fit pt-16 pb-10 px-10 max-[450px]:w-[100%] max-[450px]:h-[100vh] w-[460px] ] "
+                    >
+                        <Image height={130} className="-mb-4" src={logo} alt="logo" />
+                        <div className="text-[32px] font-semibold  ">Exam Portal</div>
+                        <div className="text-2xl w-full font-semibold ">Login</div>
+                        <div className="mt-1 w-[100%]">
+                            <TextField
+                                required
+                                onChange={(e) => {
+                                    setRollNo(e.target.value);
+                                    setHelperText("");
+                                    setError(false);
+                                }}
+                                value={RollNo}
+                                helperText={helperText}
+                                error={error}
+                                className=""
+                                sx={{
+                                    ".MuiInputBase-input": {
+                                        borderRadius: "10px",
+                                    },
+                                    "&:before, &:after": {
+                                        borderRadius: "10px",
+                                    },
+                                }}
+                                InputProps={{
+                                    style: {
+                                        borderRadius: "10px",
+                                    },
+                                }}
+                                id="myfilled-name"
+                                label="Roll No"
+                                variant="outlined"
+                                color="grey"
+                                fullWidth
+                            />
+                        </div>
+                        <div className="mt-1 w-[100%]">
+                            <TextField
+                                required
+                                onChange={(e) => {
+                                    setPass(e.target.value);
+                                    setHelperText("");
+                                    setError(false);
+                                }}
+                                type="password"
+                                value={Password}
+                                helperText={helperText}
+                                error={error}
+                                sx={{
+                                    "&:before, &:after": {
+                                        borderRadius: "10px",
+                                    },
+                                }}
+                                InputProps={{
+                                    style: {
+                                        borderRadius: "10px",
+                                    },
+                                }}
+                                id="myfilled-name"
+                                label="Password"
+                                variant="outlined"
+                                color="grey"
+                                fullWidth
+                            />
+                        </div>
+                        <button className="bg-black flex justify-center items-center  transition-all duration-150 gap-x-3 text-white w-full p-4 rounded-2xl font-semibold">
+                            <div> Sign In </div> <ArrowForwardIosIcon className="scale-75  " />
+                        </button>
+                        <div className="flex w-full justify-between">
+                            <div
+                                onClick={() => {
+                                    router.push("/admin");
+                                }}
+                                className="w-full  cursor-pointer hover:underline "
+                            >
+                                Are you an admin?
+                            </div>
+                            <div
+                                onClick={() => {
+                                    router.push("/forgotpassword");
+                                }}
+                                className="w-full text-right cursor-pointer hover:underline"
+                            >
+                                Forgot Password?
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <button className="bg-black flex justify-center items-center  transition-all duration-150 gap-x-3 text-white w-full p-4 rounded-2xl font-semibold">
-              <div> Sign In </div>{" "}
-              <ArrowForwardIosIcon className="scale-75  " />
-            </button>
-            <div className="flex w-full justify-between">
-            <div onClick={()=>{router.push("/admin")}} className="w-full  cursor-pointer hover:underline ">Are you an admin?</div>
-            <div onClick={()=>{router.push("/forgotpassword")}} className="w-full text-right cursor-pointer hover:underline">Forgot Password?</div>
-            </div>
-          </form>
-        </div>
-      </div>
-      <Backdrop
-        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={loading}
-      >
-        <CircularProgress color="inherit" />
-      </Backdrop>
-    </>
-  );
+            <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={loading}>
+                <CircularProgress color="inherit" />
+            </Backdrop>
+        </>
+    );
 }
