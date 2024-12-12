b340175d19253a0937c6fe2061fed02f37046597
anant <ab6157@dseu.ac.in>
2024-12-12 11:42:37 +0530

    [fix] : db fix

--- db.ts
+++ db.ts
@@ -1,12 +1,12 @@
-import { Pool } from 'pg';
+import { Pool } from "pg";
 
 const pool = new Pool({
-  user: "postgres",
-  password: "1234",
-  host: "localhost",
-  port: 5432, // default Postgres port
-  database: "dseu_erp" ,
-  ssl: false
+    user: "neondb_owner",
+    password: "GCekN13PVhYz",
+    host: "ep-fancy-boat-a5vodunq.us-east-2.aws.neon.tech",
+    port: 5432, // default Postgres port
+    database: "neondb",
+    ssl: true,
 });
 
 export default pool;
